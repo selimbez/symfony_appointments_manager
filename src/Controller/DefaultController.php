5ad3ab275cf2b25e5ef72e231bdb27f2b937b018
selimbez <selim924@gmail.com>
2019-09-24 18:38:03 +0200

    added redirect to default route

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="root")
+     * @param TokenStorageInterface $tokenStorage
+     * @return Response
+     */
+    public function index(TokenStorageInterface $tokenStorage)
+    {
+        if ($tokenStorage->getToken()->isAuthenticated()) {
+            return $this->redirectToRoute(in_array("ROLE_SECRETARY", $tokenStorage->getToken()->getRoleNames()) ? 'appointment_create' : 'appointment_list');
+        } else {
+            return $this->redirectToRoute('app_login');
+        }
+    }
+}
